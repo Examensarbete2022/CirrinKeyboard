96a60477e2fd40db8a32fd7ce25de9cf4b989212
DuWonHtoo <54946293+DuWonHtoo@users.noreply.github.com>
2023-01-11 14:42:50 +0100

    divided codes into smaller functions for readability.

--- Examensarbete/app/src/main/java/com/example/examensarbete/CustomView.kt
+++ Examensarbete/app/src/main/java/com/example/examensarbete/CustomView.kt
@@ -16,13 +16,21 @@ class CustomView(context: Context) : View(context) {
     var touched = false
     private var selectedCircle: Circle? = null
     private var latestInteraction : Circle? = null
-
-    //create 26 circles that sits in a circle
-    val circleList = mutableListOf<Circle>()
+    //variables for clear button and backspace button
+    private val inputs = mutableListOf<String>()
+    private var isButtonClicked = false
+    var isDeleteLatestClicked = false
+    private val backSpaceRect = Rect()
+    private val buttonRect = Rect()
+    // get screen with and height
+    val screenWidth = resources.displayMetrics.widthPixels
+    val screenHeight = resources.displayMetrics.heightPixels
     //create list of all the letters in the alphabet
     var alphabet = arrayOf('x', 'p', 'j', 'v', 'z', 'c', 'g', 'w', 'm', 's', 'a', 't', 'h', 'e', 'r', 'o', 'n', 'i', 'f', 'l', 'd', 'u', 'k', 'b', 'y', 'q')
     // string holding the letters that the user has selected
     var str = ""
+    val circleList = mutableListOf<Circle>()
+
 
     init {
         for (i in 0..25) {
@@ -39,91 +47,104 @@ class CustomView(context: Context) : View(context) {
         strokeWidth = 5f
     }
 
-    private val inputs = mutableListOf<String>()
-    private var isButtonClicked = false
-    var isDeleteLatestClicked = false
-    private val backSpaceRect = Rect()
-    private val buttonRect = Rect()
 
-    // get screen with and height
-    val screenWidth = resources.displayMetrics.widthPixels
-    val screenHeight = resources.displayMetrics.heightPixels
+
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.apply{
-            circleList.forEach {
-                drawCircle(it.x, it.y, it.radius, paint)
-            }
-            paint.textSize = 40f
-            for (i in 0..25) {
-                drawText(alphabet[i].toString(), circleList[i].x - 13, circleList[i].y + 10, paint)
-            }
 
-            drawCircle(530f, 1450f, 500f, paint)
-            drawCircle(530f, 1450f, 400f, paint)
-            Paint.Style.FILL
+            drawCircles()
+            drawAlphabet()
+            drawTextInput()
+            clearButton()
+            backSpaceButton()
 
-            if(isButtonClicked){
-                str = ""
-                isButtonClicked = false
-            }
-
-            if(isDeleteLatestClicked){
-                if(inputs.isNotEmpty()){
-                    inputs.removeAt(inputs.size-1)
-                    str = inputs.joinToString(separator = "")
-                }
-                isDeleteLatestClicked = false
-            }
-
-
-            paint.textSize = 75f
-            Paint.Style.FILL
-            canvas.drawText(str, 10f, 100f, paint)
-
-            //clear button width and height
-            val buttonWidth = 250
-            val buttonHeight = 100
-
-            //set the coordinates for the clear button and draw rect
-            val clearBottomPadding = 200
-            val clearBtnLeft = 30
-            val clearBtnTop = screenHeight - buttonHeight - 200
-            val clearBtnBottom = screenHeight - clearBottomPadding
-            buttonRect.set(clearBtnLeft, clearBtnTop, buttonWidth, clearBtnBottom)
-            canvas.drawRect(buttonRect, paint)
-
-            // Clear Button Paint
-            paint.color = Color.BLACK
-            paint.textSize = 40f
-            val text = "Clear"
-            val textWidth = paint.measureText(text)
-            val x = buttonRect.centerX() - textWidth / 2
-            val y = buttonRect.centerY() + ((paint.descent() + paint.ascent()) / 2) + 25
-            canvas.drawText(text, x, y, paint)
-
-            //backspace button width and height
-            val latestWidth = 250
-            val latestHeight = 100
-            //set the coordinates for the backspace button and draw rect
-            val backSpaceBottomPadding = 200
-            val backSpaceLeft = screenWidth - latestWidth
-            val backSpaceTop = screenHeight - latestHeight - backSpaceBottomPadding
-            val backSpaceRight = screenWidth - 30
-            val backSpaceBottom = screenHeight - backSpaceBottomPadding
-            backSpaceRect.set(backSpaceLeft, backSpaceTop, backSpaceRight, backSpaceBottom)
-            canvas.drawRect(backSpaceRect, paint)
-
-            //Backspace Button Paint
-            paint.color = Color.BLACK
-            paint.textSize = 40f
-            val latestTextInput = "Backspace"
-            val latestTextWidth = paint.measureText(latestTextInput)
-            val latestX = backSpaceRect.centerX() - latestTextWidth / 2
-            val latestY = backSpaceRect.centerY() + ((paint.descent() + paint.ascent()) / 2) + 25
-            canvas.drawText(latestTextInput,latestX, latestY, paint)
         }
     }
+
+    private fun Canvas.drawCircles() {
+        circleList.forEach {
+            drawCircle(it.x, it.y, it.radius, paint)
+        }
+        drawCircle(530f, 1450f, 500f, paint)
+        drawCircle(530f, 1450f, 400f, paint)
+        Paint.Style.FILL
+    }
+
+    private fun Canvas.drawAlphabet() {
+        paint.textSize = 40f
+        for (i in 0..25) {
+            drawText(alphabet[i].toString(), circleList[i].x - 13, circleList[i].y + 10, paint)
+        }
+    }
+
+    private fun Canvas.drawTextInput(){
+        paint.textSize = 75f
+        Paint.Style.FILL
+        drawText(str, 10f, 100f, paint)
+
+        if(isButtonClicked){
+            str = ""
+            isButtonClicked = false
+            invalidate()
+        }
+
+        if(isDeleteLatestClicked){
+            if(inputs.isNotEmpty()){
+                inputs.removeAt(inputs.size-1)
+                str = inputs.joinToString(separator = "")
+            }
+            isDeleteLatestClicked = false
+            invalidate()
+        }
+    }
+
+    private fun Canvas.clearButton() {
+        val buttonWidth = 250
+        val buttonHeight = 100
+
+        //set the coordinates for the clear button and draw rect
+        val clearBottomPadding = 200
+        val clearBtnLeft = 30
+        val clearBtnTop = screenHeight - buttonHeight - 200
+        val clearBtnBottom = screenHeight - clearBottomPadding
+        buttonRect.set(clearBtnLeft, clearBtnTop, buttonWidth, clearBtnBottom)
+        drawRect(buttonRect, paint)
+
+        // Clear Button Paint
+        paint.color = Color.BLACK
+        paint.textSize = 40f
+        val text = "Clear"
+        val textWidth = paint.measureText(text)
+        val x = buttonRect.centerX() - textWidth / 2
+        val y = buttonRect.centerY() + ((paint.descent() + paint.ascent()) / 2) + 25
+        drawText(text, x, y, paint)
+    }
+
+    private fun Canvas.backSpaceButton() {
+        //backspace button width and height
+        val latestWidth = 250
+        val latestHeight = 100
+        //set the coordinates for the backspace button and draw rect
+        val backSpaceBottomPadding = 200
+        val backSpaceLeft = screenWidth - latestWidth
+        val backSpaceTop = screenHeight - latestHeight - backSpaceBottomPadding
+        val backSpaceRight = screenWidth - 30
+        val backSpaceBottom = screenHeight - backSpaceBottomPadding
+        backSpaceRect.set(backSpaceLeft, backSpaceTop, backSpaceRight, backSpaceBottom)
+        drawRect(backSpaceRect, paint)
+
+        //Backspace Button Paint
+        paint.color = Color.BLACK
+        paint.textSize = 40f
+        val latestTextInput = "Backspace"
+        val latestTextWidth = paint.measureText(latestTextInput)
+        val latestX = backSpaceRect.centerX() - latestTextWidth / 2
+        val latestY = backSpaceRect.centerY() + ((paint.descent() + paint.ascent()) / 2) + 25
+        drawText(latestTextInput,latestX, latestY, paint)
+    }
+
+
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
@@ -180,4 +201,3 @@ class CustomView(context: Context) : View(context) {
         return true
     }
 }
-
